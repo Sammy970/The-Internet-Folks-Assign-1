02ee9004914ecc3dca19b118c2f2cc7e605222ad
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-10-15 07:03:03 +0530

    Added setup for sections in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import './App.css';
+import HeroPage from './sections/HeroPage';
+import AboutPage from './sections/AboutPage';
+import LatestArticles from './sections/LatestArticles';
+import FooterPage from './sections/FooterPage';
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <main className="bg">
+      <section>
+        <HeroPage />
+      </section>
+      <section className="bg-gray">
+        <AboutPage />
+      </section>
+      <section>
+        <LatestArticles />
+      </section>
+      <section className="bg-gray">
+        <FooterPage />
+      </section>
+    </main>
+  );
 }
 
 export default App;
